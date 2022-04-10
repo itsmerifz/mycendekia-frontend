421cb0b06b34ae154c850a57950fe8ac50d343fd
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2022-04-10 07:25:06 +0700

    add table list article

--- src/dashboard/articles/index.jsx
+++ src/dashboard/articles/index.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../../components/dashboard/header'
 import Swal from 'sweetalert2'
 import 'sweetalert2/dist/sweetalert2.css'
 import withReactContent from 'sweetalert2-react-content'
 import { BeatLoader } from 'react-spinners'
 import { BiPlus } from 'react-icons/bi'
+import { FaTrash, FaEdit } from 'react-icons/fa'
 import { Transition, Dialog } from '@headlessui/react'
-import { addArticle } from '../../../service/httpClient'
+import { addArticle, getArticles, deleteArticle } from '../../../service/httpClient'
 
 const MySwal = withReactContent(Swal)
 
@@ -27,6 +28,13 @@ export default function Articles() {
   const [year, setYear] = useState('')
   const [link, setLink] = useState('')
   const [modal, setModal] = useState(false)
+
+  useEffect(() => {
+    getArticles().then((res) => {
+      setArticles(res.data.data)
+    })
+  }, [])
+
 
   const handleAddArticle = e => {
     e.preventDefault();
@@ -60,9 +68,34 @@ export default function Articles() {
       })
   }
 
+  const handleDeleteArticle = (id) => {
+    MySwal.fire({
+      title: 'Apakah anda yakin?',
+      text: 'Anda tidak dapat mengembalikan data yang sudah dihapus!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#6fcf15',
+      cancelButtonColor: '#5e5e5e',
+      confirmButtonText: 'Ya, hapus!',
+    })
+    .then((result) => {
+      if(result.isConfirmed){
+        deleteArticle(id)
+        .then((res) => {
+          MySwal.fire({
+            icon: 'success',
+            title: res.data.message,
+          })
+          getArticles().then((res) => {
+            setArticles(res.data.data)
+          })
+        })
+      }
+    })
+  }
 
   return (
-    <div className='flex-1 w-full h-screen bg-gray-300'>
+    <div className='flex-1 w-full h-screen bg-gray-300 max-w-7xl'>
       <Header />
       <div className='flex items-center justify-between p-5 pb-0'>
         <h1 className='font-bold text-2xl text-gray-600'>Daftar Artikel</h1>
@@ -70,6 +103,78 @@ export default function Articles() {
           + Tambah Artikel
         </button>
       </div>
+
+      {/* List Article */}
+      <div className="mt-5 p-3 w-full relative overflow-x-auto items-center justify-center flex">
+        <table>
+          <thead className='uppercase bg-gray-600 text-white'>
+            <tr>
+              <th scope='col' className='px-3 py-2 border'>
+                No.
+              </th>
+              <th scope='col' className='px-3 py-2 border'>
+                Judul
+              </th>
+              <th scope='col' className='px-3 py-2 border'>
+                Author
+              </th>
+              <th scope='col' className='px-3 py-2 border'>
+                Tahun
+              </th>
+              <th scope='col' className='px-3 py-2 border'>
+                Link
+              </th>
+              <th scope='col' className='px-3 py-2 border'>
+                Aksi
+              </th>
+            </tr>
+          </thead>
+          <tbody className='border px-3 py-2 w-24 h-auto'>
+            {
+              articles.length > 0 ?
+
+              articles.map((article, index) => {
+                return (
+                  <tr key={index}>
+                    <td className='border px-3 py-2'>
+                      <div className="flex justify-center items-center">
+                        {index + 1}
+                      </div>
+                    </td>
+                    <td className='border px-3 py-2'>
+                      <p className='truncate w-72'>{article.title}</p>
+                    </td>
+                    <td className='border px-3 py-2'>
+                      <p className='truncate w-72'>{article.author}</p>
+                    </td>
+                    <td className='border px-3 py-2'>{article.year}</td>
+                    <td className='border px-3 py-2'>
+                      <p className='truncate w-48'>{article.link}</p>
+                    </td>
+                    <td className='border px-3 py-2'>
+                      <div className="flex items-center gap-6 justify-center">
+                        <button className='w-auto h-10 text-lime-500 hover:text-lime-600 transition text-2xl' type='button'>
+                          <FaEdit />
+                        </button>
+                        <button className='w-auto h-10 text-red-500 hover:text-red-600 transition text-2xl' type='button' onClick={() => handleDeleteArticle(article._id)}>
+                          <FaTrash />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                )
+              })
+              :
+              <tr>
+                <td colSpan='6' className='border px-3 py-2'>
+                  <p className='text-center text-gray-600'>Tidak ada artikel</p>
+                </td>
+              </tr>
+            }
+          </tbody>
+        </table>
+      </div>
+
       {/* Modal Add Articles */}
       {
         modal && (
